5661abad4db2c4b9f5db4ee52661a87fb3243c32
kerneyj <gamerguy9801@gmail.com>
2023-07-07 18:54:44 +0000

    updated apnet script to make multiple devices

--- apnet.sh
+++ apnet.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-ip tuntap add tapvm1 mode tap
-ip addr add 10.0.0.1/24 dev tapvm1
-ip link set tapvm1 up
+for i in $(seq 0 $(expr $1 - 1))
+do
+    ip tuntap add tapvm$i mode tap
+    ip addr add 10.0.$i.1/24 dev tapvm$i
+    ip link set tapvm$i up
+done
